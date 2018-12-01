d0868713db17ae2b94d8c9a3081c64012aa74ab2
joferbaia <nan2baia@hotmail.com>
2018-11-30 22:08:48 -0200

    first commit

---
+++ src/main/java/pom/BoardPage.java
@@ -0,0 +1,15 @@
+package pom;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class BoardPage {
+
+    public class BoardPage {
+        @FindBy (how = How.XPATH, using = "//a[@class='board-tile mod-light-background'")
+        private WebElement boardName;
+        @FindBy (how = How.XPATH, using = "//div")
+
+    }
+}
